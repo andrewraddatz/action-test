cc4006d902eb5cc211469dcef6a06671b5922005
Automated Update <automated.tracker@crontab.com>
2022-10-18 21:50:02 -0500

    Latest data: Wed Oct 19 02:50:01 AM UTC 2022

--- scrape.sh
+++ scrape.sh
@@ -4,6 +4,7 @@ timestamp=$(date -u)
 echo "${timestamp}" > lastrun.txt
 git config user.name "Automated Update"
 git config user.email "automated.tracker@crontab.com"
+git pull
 git add -A
 git commit -m "Latest data: ${timestamp}" || exit 0
 git push
